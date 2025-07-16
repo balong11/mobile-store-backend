c6af659da88bbfc475c673fb981db73a2b4c4ca3
dotien <dobatien94@gmail.com>
2025-07-16 19:47:27 +0700

    tt

--- src/apps/controllers/productController.js
+++ src/apps/controllers/productController.js
@@ -1,6 +1,11 @@
 const ProductModel = require("../models/Product");
 exports.index = async (req, res) => {
-  const products = await ProductModel.find().populate("cat_id");
+  // const products = await ProductModel.find().populate("cat_id").limit(10);
+  const limit = 10;
+  const page = req.query.page || 1;
+  const skip = page * limit - limit;
+  const products = await ProductModel.find().populate({ path : "cat_id"}).sort({_id : -1}).skip(skip).limit(limit);
+
   return res.render("admin/products/product", { products });
 };
 exports.create = (req, res) => {
